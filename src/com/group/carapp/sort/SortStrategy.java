4fcd81a0e24543d6f9f7350cdb574cf67c72f9b1
milanafil <125284843+miilanafil@users.noreply.github.com>
2026-09-11 18:46:46 +0300

    добавлен интерфейс SortStrategy

---
+++ src/com/group/carapp/sort/SortStrategy.java
@@ -0,0 +1,10 @@
+package com.group.carapp.sort;
+
+import com.group.carapp.model.Car;
+
+import java.util.List;
+
+public interface SortStrategy {
+    void sort(List<Car> cars);
+    String getName();
+}
